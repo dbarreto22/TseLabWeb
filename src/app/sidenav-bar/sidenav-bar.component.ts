0c0091c76366796ee3c9c8813fb8acbdbf700c80
Carlox <caralnuma@gmail.com>
2019-06-19 00:54:35 -0300

    estilo

--- src/app/sidenav-bar/sidenav-bar.component.ts
+++ src/app/sidenav-bar/sidenav-bar.component.ts
@@ -143,7 +143,7 @@ export class SidenavBarComponent implements OnInit {
     this.storage.pruebaObs();
   }
   donar() {
-    this.storage.getAllHechos().subscribe(res => console.log(res));
+    this.router.navigate(['/donar']);
   }
 
   irGraficas() {
