646bcf805c7eab300fd26cbd567550e4b2e139cb
Carlox <caralnuma@gmail.com>
2019-06-26 18:42:15 -0300

    Con pagina de inicio mejorada

---
+++ src/app/Usuarios/clases/Preview.ts
@@ -0,0 +1,10 @@
+import { stringify } from '@angular/core/src/render3/util';
+
+export class Preview {
+    constructor() {
+    }
+    public title: string;
+    public description: string;
+    public image: string;
+    public url: string;
+}
--- src/app/primer-pagina/primer-pagina.component.ts
+++ src/app/primer-pagina/primer-pagina.component.ts
@@ -4,10 +4,14 @@ import { Component, OnInit } from '@angular/core';
 import { State, CompositeFilterDescriptor } from "@progress/kendo-data-query";
 import { SelectableSettings, RowArgs, PageChangeEvent } from "@progress/kendo-angular-grid";
 
-import { Observable } from "rxjs";
+import { Observable, BehaviorSubject } from "rxjs";
 import { Hechos } from 'src/app/Usuarios/clases/hechos';
+import { HechosImg } from 'src/app/Usuarios/clases/hechosImg';
 import { ApiServiceService } from '../api-service.service';
 import { StorageService } from '../storage.service';
+import { Preview } from '../Usuarios/clases/Preview';
+import { checkAndUpdateElementDynamic } from '@angular/core/src/view/element';
+
 
 @Component({
   selector: 'app-primer-pagina',
@@ -21,24 +25,86 @@ export class PrimerPaginaComponent implements OnInit {
   public subbmiter;
   public url;
   public usuarioAlta;
-  public hechos: Observable<Array<Object>>;
-//  public hechos;
+  public hechos: Observable<Array<HechosImg>>;
+  public hechosImagenes : BehaviorSubject<Array<HechosImg>> = new BehaviorSubject<Array<HechosImg>>([]);
+  public hechosCard = new Array<HechosImg>();
   public checked = false;
   public filter: CompositeFilterDescriptor;
   selectedValue: any[];
   public checkboxOnly = true;
   public selectableSettings: SelectableSettings;
   public dialogOpened = false;
-  constructor(  private apiService: ApiServiceService, private storage:StorageService) {
-      this.hechos = this.apiService.gethechosByEstados("VERIFICADO");
-      this.hechos.subscribe(
-        (res) =>{ console.log(res)},
-        ee => {
-           console.log(ee)
-        }
-      )
+  public hecho = new HechosImg();
 
-    }
+  constructor(private apiService: ApiServiceService, private storage: StorageService) 
+  {
+    this.hechosImagenes.subscribe(items=>{
+      console.log('LPM');
+      console.log(items);
+      console.log(this.hechosCard);
+      this.hechosCard=items;
+    },err=>console.log(err))
+  //  this.inicializarlista(this.hechosImagenes);
+    this.hechos = this.apiService.gethechosByEstadosImg("VERIFICADO");
+    this.hechos.subscribe
+    (
+      (res) => 
+    { console.log('servicio getHEchos')
+    console.log(res);
+      /*   res.forEach(element => {
+          this.apiService.getPreview(element.url).subscribe(
+            (respuesta: Preview) => {
+              console.log(element);
+              console.log('Entre al preview');
+              console.log(respuesta.image);
+              if(respuesta.image=="")
+                element.img="assets/1024px-No_image_available.svg.png";
+              else
+                element.img = respuesta.image;
+          console.log('element.image after load');
+          console.log(element.img);
+         });
+        },err=>
+        console.log(err)*/
+// /*        
+        this.hecho.clear();
+        res.forEach(element => {
+          console.log('elemento de hechos');
+          console.log(element);
+          console.log('llegara?')
+          this.apiService.getPreview(element.url).subscribe(
+            (respuesta: Preview) => {
+              if(respuesta.image=="")
+                element.img="assets/1024px-No_image_available.svg.png"
+              else
+                element.img = respuesta.image;
+                console.log('adding element')
+                //this.addElement(element);
+                this.hechosImagenes.next(res);
+            },
+            err=>{console.log(err);
+            element.img="assets/1024px-No_image_available.svg.png";
+            console.log('adding element');
+            this.hechosImagenes.next(res);
+          }
+            );
+        });
+
+      },
+      ee => {
+        console.log(ee)
+  //  */  
+         
+         console.log('********PrimerPagina- getHechos : hechosImagenes - FIN ForEach ');
+         this.hechos.forEach(as=>console.log(as.values()));
+
+        }
+    )
+    console.log('********PrimerPagina- getHechos : hechosImagenes - FIN ');
+    console.log(this.hechos);
+    // this.cargarPreview()
+      }
+    
 
   ngOnInit() {
     this.storage.setRol(localStorage.getItem('rol'));
@@ -77,17 +143,36 @@ export class PrimerPaginaComponent implements OnInit {
             this.titulo = hecho.titulo;
             console.log("***************IDHECHO********************");
             console.log(this.id);
-            localStorage.setItem("idHecho",this.id);
+            localStorage.setItem("idHecho", this.id);
           }
         })
 
       },
       err => {
-        console.error("Se rompe en hechos.component",err);
+        console.error("Se rompe en hechos.component", err);
         //this.apiService.mensajeConError(err);
       }
     )
   }
 
+  inicializarlista(aux:Array<HechosImg>){
+    aux.length=0;
+  }
+  cargarPreview() {
+  /*  let grabity = require("grabity");
+ 
+    (async () => {
+      let it = await grabity.grabIt("https://github.com/e-oj/grabity");
+     
+      console.log(it);
+    })();
+  */}
 
+  addElement(item){
+    console.log('addElement')
+    console.log(item);
+    console.log('HechosCardAntes')
+    console.log(this.hechosCard);
+    this.hechosImagenes.next(item);
+  }
 }
